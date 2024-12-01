49a9d1088cc6709a327ffe1d7db6d84e0d9c57b8
Max Boone <max.boone@maxem.energy>
2024-12-01 21:28:33 +0100

    feat: benches

--- ADM/assignments/assignment_04/scripts/bench.sh
+++ ADM/assignments/assignment_04/scripts/bench.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+for f in l_discount.csv l_extendedprice.csv l_orderkey.csv l_tax.csv data.csv; do
+	for i in $(seq 1000 5000 100000); do
+		stderr_output=$(./bin/branching $i ./data/$f 2>&1 >/dev/null)
+		echo $stderr_output
+		stderr_output=$(./bin/condition $i ./data/$f 2>&1 >/dev/null)
+		echo $stderr_output
+	done
+done
